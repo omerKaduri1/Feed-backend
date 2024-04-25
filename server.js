510bc3cc5b929eabe78a5519db81de57d96b2522
omerKaduri1 <kaduriomer@gmail.com>
2024-04-25 14:00:13 +0300

    Add server js

---
+++ server.js
@@ -0,0 +1,51 @@
+import http from 'http'
+import path from 'path'
+import cors from 'cors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.static('public'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve('public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:3030',
+            'http://localhost:3030',
+            'http://127.0.0.1:5173',
+            'http://localhost:5173'
+        ],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
+
+import { commentRoutes } from './api/comment/comment.routes.js'
+import { logger } from './services/logger.service.js'
+// import { authRoutes } from './api/auth/auth.routes.js'
+// import { userRoutes } from './api/user/user.routes.js'
+import { setupSocketAPI } from './services/socket.service.js'
+import { setupAsyncLocalStorage } from './middlewares/setupAls.middleware.js'
+
+
+app.all('*', setupAsyncLocalStorage)
+
+app.use('/api/comment', commentRoutes)
+// app.use('/api/auth', authRoutes)
+// app.use('/api/user', userRoutes)
+setupSocketAPI(server)
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'))
+})
+
+
+const port = process.env.PORT || 3030
+server.listen(port, () => {
+    logger.info('Server is running on port: ' + port)
+})
